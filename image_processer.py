512c4dfb6ed852eb974b6354578eca8e79546195
root <root@Scott-Desktop.localdomain>
2018-02-20 20:21:29 -0500

    Image processor ready to test, finished template model, added image library requirement

--- image_processer.py
+++ image_processer.py
@@ -1,13 +1,55 @@
 from PIL import Image
 from models.template import Template
+from azure import Azure
 
 class ImageProcesser:
     """
     Handles processing of images. Takes images and boxes on them, and sends them to Azure for processing, returning the result.
 
     Args:
-        template (Template|None): The template to use when processing the image
+        template (Template): The template to use when processing the image
+        service (Azure): A wrapper for a Computer Vision webservice
     """
-    def __init__(self, template=None):
+    def __init__(self, template, service):
         self.template = template
+        self.service = service
 
+    def processImage(self, image):
+        """
+        Applies template to given image, returning values found
+
+        Args:
+            image (binary): binary for an image
+
+        Returns:
+            crops (dictionary): mapping from the template box names to values found in that box
+        """
+
+        image_handle = io.BytesIO(image)
+        image = Image.frombytes(image_handle)
+
+        crops = {}
+        # crop out each box, and run it through image recognition
+        for box in self.template.boxes:
+            area = box['descr']
+            datatype = box['datatype']
+            subimage = image.crop(area[0], area[1], area[2], area[3])
+            subimagebytes = io.BytesIO(b'')
+            subimage.save(subimagebytes, 'png')
+            raw_result = self.service.recognizeHandwriting(subimagebytes)
+            crops['name'] = self._processResult(raw_result, datatype)
+
+        return crops
+
+    def _processResult(self, result, datatype):
+        """
+        Converts given result to given datatype
+
+        Args:
+            result (str): text parsed from image
+            datatype (str): desired type to convert text parsed from image to
+        """
+        return {
+            'int': lambda res: int(res),
+            'str': lambda res: res,
+        }[datatype](result)
--- models/template.py
+++ models/template.py
@@ -1,3 +1,34 @@
 class Template:
-    def __init__(self):
-        pass
+    """
+    Template to be used for matching against images.
+
+    Args:
+        template (Template): initializes the template to be a clone of the other template
+    """
+    def __init__(self, template=None):
+        if template:
+            self.boxes = template.boxes
+        else:
+            self.boxes = []
+
+    def addBox(self, name, descr, datatype):
+        """
+        adds a box to the template
+
+        Args:
+            name (str):                        A string to identify given box. Should be unique
+            descr (tuple<int, int, int, int>): Top left coord, top right coord, width, height of box
+            datatype (str):                    Name of datatype to be found in box.
+
+        Side Effects:
+            Appends a dictionary created from the parameters to the template boxes list
+        """
+        assert name not in map(lambda box: box['name'], self.boxes)
+
+        newBox = {
+            'name': name,
+            'descr': descr,
+            'datatype': datatype
+        }
+
+        self.boxes.append(newBox)
